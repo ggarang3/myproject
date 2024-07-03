5082d010d8eaeb312d34eba01989fa37d3280ac0
Gai <ggarang24@gmail.com>
2024-07-03 15:38:36 +0800

    updated Java Script

--- index/index.js
+++ index/index.js
@@ -1,3 +1,5 @@
 document.getElementById("demo").style.color ="red"
  onclick="helloWorld()"
- 
+ document.getElementById("header").innerText = 'hello Javascript';
+document.getElementById("aside").style.backgroundColor = "green"
+
